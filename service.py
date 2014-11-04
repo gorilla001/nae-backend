b8257fba7c71b7c21372e8ce22ecd8ab9edbc097
niuminguo <none@none>
2014-11-04 16:51:04 +0800

    修改bug
    --HG--
    branch : minguon-dev

--- service.py
+++ service.py
@@ -52,7 +52,7 @@ class ProcessLauncher(object):
 	pid = os.fork()
 	if pid == 0:
 	    self._child_process(wrap.server)
-	wrap.children.add(pid)
+	    wrap.children.add(pid)
 
     def launch_server(self,server,workers=1):
 	wrap = ServerWrapper(server,workers)
