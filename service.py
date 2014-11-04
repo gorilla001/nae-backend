28355229bdb562fe88b493a6a1a87385537500ae
niuminguo <none@none>
2014-11-04 11:49:31 +0800

    调试信息
    --HG--
    branch : minguon-dev

--- service.py
+++ service.py
@@ -6,7 +6,9 @@ class Service(object):
 	self.tg = threadgroup.ThreadGroup()
     def start(self):
 	self.tg.start_thread(self.run_service,self.service)
+
     @staticmethod
     def run_service(service):
+	print 'run service'
 	service.start()
 	service.wait()	
