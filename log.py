0f7554b1a1a97ff21881647130ddb08e1c37ce06
niuminguo <none@none>
2014-11-05 13:55:34 +0800

    去掉调试信息
    --HG--
    branch : minguon-dev

--- log.py
+++ log.py
@@ -2,17 +2,16 @@ import logging
 
 def getlogger():
     logger=logging.getLogger()
-    level=logging.NOTSET
     hdlr=logging.FileHandler('/var/log/jaecpn/jaecpn.log')
     formatter=logging.Formatter('%(asctime)s %(levelname)s %(message)s','%Y-%m-%d %H:%M:%S')
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
-    logger.setLevel(level)
     return logger
 
 class WSGILogger(object):
-    def __init__(self,logger):
+    def __init__(self,logger,level=logging.INFO):
         self.logger = logger
+        self.level = level
+
     def write(self, msg):
-        self.logger.log(logging.NOTSET, msg.rstrip())
-        #self.logger.debug(msg.rstrip())
+        self.logger.log(self.level, msg.rstrip())
