3ef231a493f92e839576cc8100f3e93f7a2df364
niuminguo <none@none>
2014-11-05 09:45:19 +0800

    修改bug
    --HG--
    branch : minguon-dev

--- service.py
+++ service.py
@@ -18,51 +18,53 @@ class WSGIService(object):
 				self.port,
 				self.logger)
     def start(self):
-	self.server.start()
+	    self.server.start()
 
     def stop(self):
-	self.server.stop()
+	    self.server.stop()
 
     def wait(self):
-	self.server.wait()
+	    self.server.wait()
 				
 class ServerWrapper(object):
     def __init__(self,server,workers):
-	self.server = server
-	self.workers = workers
-	self.children = set()    
+	    self.server = server
+	    self.workers = workers
+	    self.children = set()    
+
+class Launcher(object):
+    @staticmethod
+    def run_service(service)
+        service.start()
+        service.wait()
 
 class ProcessLauncher(object):
     def __init__(self):
-	self._services=[]
-	self.tg = threadgroup.ThreadGroup()
-
-    @staticmethod
-    def run_server(server):
-	server.start()
-	server.wait()	
+	    self._services=[]
+	    self.tg = threadgroup.ThreadGroup()
 
     def _child_process(self,server):
-	eventlet.hubs.use_hub()
+	    eventlet.hubs.use_hub()
 
-	#gt=self.tg.start_thread(self.run_server,server)
-	gt = eventlet.spawn(self.run_server, server)
-	self._services.append(gt)
-	#self.run_server(server)
+	    #gt=self.tg.start_thread(self.run_server,server)
+	    #gt = eventlet.spawn(self.run_server, server)
+	    #self._services.append(gt)
+	    #self.run_server(server)
+        launcher = Launcher()
+        launcher.run_service(server)
 
     def _start_child(self,wrap):
-	pid = os.fork()
-	#if pid == 0:
-	#    self._child_process(wrap.server)
-	#    os._exit(0)
-	self._child_process(wrap.server)
-	wrap.children.add(pid)
+	    pid = os.fork()
+	    if pid == 0:
+	        self._child_process(wrap.server)
+	        os._exit(0)
+	    wrap.children.add(pid)
 
     def launch_server(self,server,workers=1):
-	wrap = ServerWrapper(server,workers)
-	while len(wrap.children) < wrap.workers:
-	    self._start_child(wrap)
+	    wrap = ServerWrapper(server,workers)
+	    while len(wrap.children) < wrap.workers:
+	        self._start_child(wrap)
 
     def wait(self):
-	for service in self._services:
+	    for service in self._services:
 	    service.wait()
