661fbc924c361ff8f2992bcd35f2696ca31bb654
niuminguo <none@none>
2014-11-18 14:12:33 +0800

    初始化成daemon时纪录fork出错日志
    --HG--
    branch : minguon-dev

--- daemon.py
+++ daemon.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 
 import sys,os
+import logging
+
+LOG=logging.getLogger('eventlet.wsgi.server')
 
 class Daemon():
-    def __init__(self,stdout="/var/log/jaecpn/jaecpn.log",stderr="/var/log/jaecpn/jaecpn.log"):
+    def __init__(self):
         self.pid_file='jaecpn.pid'
+	self.stdout="/var/log/jaecpn/jaecpn.log"
+	self.stderr="/var/log/jaecpn/jaecpn.log"
     def initDaemon(self):
         try:
             pid = os.fork()
             if pid > 0:
                 sys.exit(0)
         except OSError,e:
+	    LOG.error("fork error")
             sys.exit(1)
     
         os.chdir(os.path.dirname(__file__))
@@ -22,12 +28,13 @@ class Daemon():
             if pid > 0:
                 sys.exit(0)
         except OSError,e:
+	    LOG.error("fork error")
             sys.exit(1)
 	
 	for f in sys.stdout,sys.stderr:
 	    f.flush()
-        so = file(stdout,'a+')
-	se = file(stderr,'a+',0) 
+        so = file(self.stdout,'a+')
+	se = file(self.stderr,'a+',0) 
 	os.dup2(so.fileno(),sys.stdout.fileno())
 	os.dup2(se.fileno(),sys.stderr.fileno())
 
