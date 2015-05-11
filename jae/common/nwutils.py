85fa1feaaae4fffc4c1e99856c66c41a64aee1e4
niuminguo <none@none>
2015-05-11 22:06:55 +0800

    fixed
    --HG--
    branch : minguon-dev

--- jae/common/nwutils.py
+++ jae/common/nwutils.py
@@ -1,4 +1,3 @@
-# import commands
 from jae.common import cfg
 from jae.common import log as logging
 from jae.common import cmdutils
@@ -9,9 +8,6 @@ import subprocess
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
-#TEMP_PATH="/tmp"
-
-#NET_SCRIPT_PATH="/etc/sysconfig/network-scripts/"
 
 DEFAULT_NET_MASK = "255.255.255.0"
 
@@ -60,59 +56,38 @@ def set_fixed_ip(uuid, addr):
 
         """Get container's pid namespace"""
         LOG.info("Get container's namespace pid")
-        #pid = subprocess.check_output("sudo docker inspect --format '{{.State.Pid}}' %s" % uuid, shell=True)
         pid = cmdutils.call("sudo docker inspect --format '{{.State.Pid}}' %s" % (uuid,))
         LOG.info("Pid is %s" % pid.strip())
 
         """Add internal veth web-int to container"""
         LOG.info("Attach internal %s to container" % veth_int)
-        #subprocess.check_call("sudo ip link set netns %s dev %s" % (pid.strip(), veth_int), shell=True)
         cmdutils.cast("sudo ip link set netns %s dev %s" % (pid.strip(), veth_int))
 
         """Rename internal veth web-int to eth0"""
         LOG.info("Rename internal veth %s to eth0" % veth_int)
-        #subprocess.check_call("sudo nsenter -t %s -n ip link set %s name eth0" % (pid.strip(), veth_int), shell=True)
         cmdutils.cast("sudo nsenter -t %s -n ip link set %s name eth0" % (pid.strip(), veth_int))
-        #subprocess.check_call("sudo docker exec --cap-add=NET_ADMIN %s ip link set %s name eth0" % (uuid,veth_int), shell=True)
 
         """Set internal veth to UP"""
         LOG.info("UP internal veth eth0")
-        #subprocess.check_call("sudo nsenter -t %s -n ip link set eth0 up" % pid.strip(), shell=True)
         cmdutils.cast("sudo nsenter -t %s -n ip link set eth0 up" % (pid.strip(),))
-        #subprocess.check_call("sudo docker exec  %s ip link set eth0 up" % (uuid,), shell=True)
 
         """Set external veth to UP"""
         LOG.info("UP external %s" % veth_ext)
-        #subprocess.check_call("sudo ip link set %s up" % veth_ext, shell=True)
         cmdutils.cast("sudo ip link set %s up" % (veth_ext,))
 
         """Set fixed ip to internal veth `eth0`"""
         IP_ADDR = "%s/%s" % (addr, DEFAULT_NET_MASK)
 
         LOG.info("Attach fixed IP to internal veth eth0")
-        #subprocess.check_call("sudo nsenter -t %s -n ip addr add %s dev eth0" % (pid.strip(), IP_ADDR), shell=True)
         cmdutils.cast("sudo nsenter -t %s -n ip addr add %s dev eth0" % (pid.strip(), IP_ADDR))
-        #subprocess.check_call("sudo docker exec  %s ip addr add %s dev eth0" % (uuid, IP_ADDR), shell=True)
 
         """Set default gateway to br0's gateway"""
         DEFAULT_GATEWAY = get_default_gateway()
         LOG.info("Set default gateway to %s" % DEFAULT_GATEWAY)
-        #subprocess.check_call("sudo nsenter -t %s -n ip route del default" % pid.strip(),shell=True)
-        #subprocess.check_call(
-        #    "sudo nsenter -t %s -n ip route add default via %s dev eth0" % (pid.strip(), DEFAULT_GATEWAY), shell=True)
         cmdutils.cast("sudo nsenter -t %s -n ip route add default via %s dev eth0" % (pid.strip(), DEFAULT_GATEWAY))
-        #subprocess.check_call(
-        #    "sudo docker exec  %s ip route add default via %s dev eth0" % (uuid, DEFAULT_GATEWAY), shell=True)
-
-
-        #"""Flush gateway's arp caching"""
-        #LOG.info("Flush gateway's arp caching")
-        #subprocess.check_call("sudo nsenter -t %s -n -- arping -c 1 -s %s %s" % (pid.strip(),addr,DEFAULT_GATEWAY),shell=True)
 
         """Ping gateway"""
         LOG.info("Flush gateway's arp caching")
-        #subprocess.check_call("sudo nsenter -t %s -n ping -c 3 %s" % (pid.strip(), DEFAULT_GATEWAY), shell=True)
-        #subprocess.check_call("sudo docker exec  %s ping -c 3 %s" % (uuid, DEFAULT_GATEWAY), shell=True)
         cmdutils.cast("sudo nsenter -t %s -n ping -c 3 %s" % (pid.strip(), DEFAULT_GATEWAY))
 
     except subprocess.CalledProcessError:
@@ -123,7 +98,6 @@ def service_init(uuid, user_id, repos):
     try: 
         LOG.info("Start maven packaging...")
         root_path = "/home/jae/%s/%s/www/%s" % (user_id, uuid[:12], os.path.basename(repos)) 
-        #subprocess.check_call("sudo" )
         cmdutils.cast("sudo")
     except subprocess.CalledProcessError:
         """Reraise the exception, the caller will catch it"""
@@ -133,13 +107,11 @@ def host_init(uuid):
     try:
         """Get container's pid namespace"""
         LOG.info("Get container's namespace pid")
-        #pid = subprocess.check_output("sudo docker inspect --format '{{.State.Pid}}' %s" % uuid, shell=True)
         pid = cmdutils.call("sudo docker inspect --format '{{.State.Pid}}' %s" % (uuid,))
         LOG.info("Pid is %s" % pid.strip())
 
         """Startup Host Init"""
         LOG.info("Init host")
-        #subprocess.check_call("sudo docker exec %s /usr/local/bin/host.init" % uuid, shell=True)
         cmdutils.cast("sudo docker exec %s /usr/local/bin/host.init" % (uuid,))
     except subprocess.CalledProcessError:
         """Raise the exception, the caller will be catch it"""
@@ -157,11 +129,9 @@ def delete_virtual_interface(uuid):
 
     try:
         LOG.info("Delete interface %s from bridge br0" % veth_ext)
-        #subprocess.check_call("sudo brctl delif br0 %s" % veth_ext, shell=True)
         cmdutils.cast("sudo brctl delif br0 %s" % (veth_ext,))
 
         LOG.info("Delete interface %s" % veth_ext)
-        #subprocess.check_call("sudo ip link del %s" % veth_ext, shell=True)
         cmdutils.cast("sudo ip link del %s" % (veth_ext,))
     except subprocess.CalledProcessError:
         raise
